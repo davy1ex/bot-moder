487a0ba68edfca9560ae5e7a087ad4afbeb3899d
davy1ex <32157455+davy1ex@users.noreply.github.com>
2018-04-09 15:50:23 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,8 @@ else:
     print("please login")
 
 # списко айди тех, кто может карать банами (записывать каждое айди в кавычках (неважно двойные или одинарные) и через запятую)
-moderators = ["154597302", "415754216"]
+#moderators = ["айди номер1", "айди номер2"]
+moderators = eval(open("moderators.txt", "r").read())
 
 
 def send_msg(chat_id, text, forward_messages = None):
@@ -69,12 +70,25 @@ def kick(chat_id, first_name = None, last_name =  None, id = None):
             pass
 
 def add_new_moder(body, chat_id):
+    """добавляет нового модера"""
     if len(body) > 2:
         moderators.append(str(get_user_id(first_name= body[1], last_name= body[2], chat_id = chat_id)))
+        open("moderators.txt", "w").write(str(moderators))
+        send_msg(chat_id, text="Добавил", forward_messages=forward_message)
 
     elif len(body) == 2:
             moderators.append(body[1])
+            open("moderators.txt", "w").write(str(moderators))
+            send_msg(chat_id, text="Добавил", forward_messages=forward_message)
 
+
+def send_moder_list(chat_id, forward_message):
+    text = ''
+    for user in users_list(chat_id):
+        print(user)
+        if str(user["uid"]) in moderators:
+            text += user["first_name"] + ' ' + user["last_name"] + '\n'
+    send_msg(chat_id, text = text, forward_messages = forward_message)
 
 # главная часть кода (да, я горазд писать кривые (но рабочие) алгоримты
 if __name__ == '__main__':
@@ -82,26 +96,32 @@ if __name__ == '__main__':
         response = get_msg(bot)
         if response != None:
             body = response["items"][0]["body"]
-            chat_id = response["items"][0]["chat_id"]
-            forward_message = response["items"][0]["id"]
-            if "!кик" in response["items"][0]["body"].lower():
-                if str(response["items"][0]["user_id"]) in moderators:
-                    body = body.split(" ")
-                    if len(body) >= 3:
-                        kick(first_name= body[1], last_name= body[2], chat_id = chat_id)
-                    elif len(body) == 2:
-                        kick(id = body[1], chat_id = chat_id)
-                else:
-                    send_msg(chat_id, text = "У тебя нет прав на это", forward_messages = forward_message)
-            elif "добавить" in body:
-                if str(response["items"][0]["user_id"]) in moderators:
-                    body = body.split(" ")
-                    chat_id = response["items"][0]["chat_id"]
-                    add_new_moder(body = body, chat_id = chat_id)
-                    print(moderators)
-                else:
-                    send_msg(chat_id, text="У тебя нет прав на это", forward_messages= forward_message)
-            elif body == "!помощь":
+            if "chat_id" not in response["items"][0]:
+                continue
+            else:
                 chat_id = response["items"][0]["chat_id"]
-                send_msg(chat_id, text="""кикнуть проказника(цу) - \"!кик Иван Пупкин\\\!кик 123456789\""
-                            \nдобавить нового модера - \"!добавить Иван Пупкин\\!добавить 123456789\"""", forward_messages = forward_message)
+                forward_message = response["items"][0]["id"]
+                if "!кик" in response["items"][0]["body"].lower():
+                    if str(response["items"][0]["user_id"]) in moderators:
+                        body = body.split(" ")
+                        if len(body) >= 3:
+                            kick(first_name= body[1], last_name= body[2], chat_id = chat_id)
+                        elif len(body) == 2:
+                            kick(id = body[1], chat_id = chat_id)
+                    else:
+                        send_msg(chat_id, text = "У тебя нет прав на это", forward_messages = forward_message)
+                elif "!добавить" in body or "добавь" in body:
+                    if str(response["items"][0]["user_id"]) in moderators:
+                        body = body.split(" ")
+                        chat_id = response["items"][0]["chat_id"]
+                        add_new_moder(body = body, chat_id = chat_id)
+                        print(moderators)
+                    else:
+                        send_msg(chat_id, text="У тебя нет прав на это", forward_messages= forward_message)
+                elif body == "!помощь":
+                    chat_id = response["items"][0]["chat_id"]
+                    send_msg(chat_id, text="""кикнуть проказника(цу) - \"!кик Иван Пупкин\\\!кик 123456789\""
+                                \nдобавить нового модера - \"!добавить Иван Пупкин\\!добавить 123456789\"""", forward_messages = forward_message)
+
+                elif body == "!модеры":
+                    send_moder_list(chat_id, forward_message = forward_message)
